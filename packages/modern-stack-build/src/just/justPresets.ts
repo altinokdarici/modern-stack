4c913315baad6f84f11568db67d46498efd52c32
Altinok Darici <altinokd@microsoft.com>
2020-04-04 14:23:00 -0700

    Fix preset scripts

--- packages/modern-stack-build/src/just/justPresets.ts
+++ packages/modern-stack-build/src/just/justPresets.ts
@@ -1,5 +1,11 @@
-import { taskPresets } from 'just-scripts';
+import { task, tscTask, cleanTask, series } from 'just-scripts';
+
+const lib = () => {
+    task('clean', cleanTask());
+    task('build', tscTask({ outDir: 'lib' }));
+    task('rebuild', series('clean', 'build'));
+};
 
 export const justPresets = {
-    lib: taskPresets.lib,
+    lib,
 };
