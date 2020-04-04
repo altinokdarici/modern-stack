25a751978af18771824e7cfab4a8d034dc3d197d
Altinok Darici <altinok.darici@outlook.com>
2020-04-04 01:27:23 -0700

    Fix tests (#3)
    * Fix tests
    
    * Change files
    
    Co-authored-by: Altinok Darici <altinokd@microsoft.com>

--- packages/build-graph/just.config.ts
+++ packages/build-graph/just.config.ts
@@ -1,3 +1,10 @@
-import { taskPresets } from 'just-scripts';
+import { taskPresets, jestTask, task } from 'just-scripts';
 
 taskPresets.lib();
+
+task(
+    'test',
+    jestTask({
+        passWithNoTests: true,
+    }),
+);
